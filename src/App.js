3f11d4ae53be457107a963bcf7fac3c2b500cbd2
VaroonSB <varoonsb1311@gmail.com>
2023-06-15 16:33:20 +0530

    added expense items, used props and composition

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
+import Card from "./components/Card";
+import ExpenseItem from "./components/ExpenseItem";
+import "./components/Expenses.css";
+
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 29),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
   return (
     <div>
       <h2>Let's get started!</h2>
+      <p>Hello,its me playing around</p>
+      <Card className="expenses">
+        {expenses.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+      </Card>
     </div>
   );
 }
